24e00c224d1163a05fe0da1e55afbbce6775362d
Gisnack <simon.peiskar@gmail.com>
2023-10-31 18:53:25 +0100

    HS

---
+++ src/main/java/com/example/zeitmanagement/Scenengerippe_Controller.java
@@ -0,0 +1,48 @@
+package com.example.zeitmanagement;
+
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+
+public class Scenengerippe_Controller {
+
+    @FXML
+    void Calander_Button(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Menu_File_AdminLogin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Menu_File_Close(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Menu_Profile_Logout(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Menu_Profile_YourProfile(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Overview_Month_Button(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Overview_Year_Button(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Time_REC_Button(ActionEvent event) {
+
+    }
+
+}
